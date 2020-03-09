6e667dac516c1f4727dcedea77109a9476e018cd
Nilesh Pawar <nilesh.p.pawar@gmail.com>
2020-03-09 21:32:33 +0000

    Good example to run standalone test

---
+++ README.md
@@ -0,0 +1,5 @@
+You can start chromedriver by running following command as well
+
+    - chromedriver --port=4444 --url-base=/wd/hub --verbose
+
+    
--- standalone.js
+++ standalone.js
@@ -3,8 +3,9 @@ const sync = require('@wdio/sync').default
 
 remote({
     runner: false,
+    scripts:"/node_modules/.bin/chromedriver --port=4444 --url-base=/wd/hub --verbose",
     outputDir: __dirname,
-    services:["chromedriver"],
+    // services:["wdio-chromedriver-service"],
     capabilities: {
         browserName: 'chrome'
     }
@@ -22,6 +23,6 @@ remote({
     var urlTitle = browser.getUrlAndTitle();
     console.log("URL : " + urlTitle.url.then(url => url) + " -- Title : " + urlTitle.title.then(title => title));
     //browser.debug()
-    browser.findElements('tagName','div')
+    // browser.findElements('tagName','div')
     browser.deleteSession()
 }))
